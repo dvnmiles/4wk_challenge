abf6dbcdad3717748582c620c42e24641d4a6185
Your Name <you@example.com>
2021-10-07 07:11:56 -0700

    updated HTML and JS files

--- assets/script/questions.js
+++ assets/script/questions.js
@@ -1,4 +1,3 @@
-// creating an array and passing the number, questions, options, and answers
 let questions = [
     {
     numb: 1,
@@ -55,17 +54,16 @@ let questions = [
       "eXamine Multiple Language"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+
+  {
+    numb: 6,
+    question: "In the TV series Knight Rider, what programming language was used to program the autonomous vehicle?",
+    answer: "KITT",
+    options: [
+      "C++",
+      "Ada.",
+      "KITT",
+      "Eiffel"
+    ]
+  },
 ];
